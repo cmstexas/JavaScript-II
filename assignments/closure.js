6d6b75fad3cbbbbc6cab2f29c1225e1d390b3c66
cmstexas <crystal.m.soria@gmail.com>
2019-06-11 17:46:28 -0500

    javascript II

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,18 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
 
+function firstName(name) { 
+  var speak = "Hey, " + name + "!!!"; 
+  var sayName = function() {
+      var joke = speak + " Knock Knock!";
+      console.log(speak); 
+  };
+  return sayName; 
+}
+
+var sayMyName = firstName("Crystal");
+sayMyName();
+
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
